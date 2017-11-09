09ab5b30c202d70f91b40181499dcfd2d7ddee88
Driggs88 <riggs_david@hotmail.com>
2017-11-08 21:02:54 -0500

    Route parameters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { Routes, RouterModule } from "@angular/router";
+import { FormsModule }   from '@angular/forms';
 
+
+const routes: Routes = [
+  { path: '', component: ContactListComponent },
+  { path: 'contact/:id', component: ContactComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent,
+    ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+  FormsModule,
+  RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
